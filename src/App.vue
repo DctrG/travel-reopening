7b13ddbbe44cb8a16857dc2893ce4e0c05e7689a
Yuri Kovalov <yuri@yurikoval.com>
2020-08-12 17:55:52 +0800

    Extract default layout into view

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app" class="flex flex-col h-screen font-body text-gray-800">
-    <div class="mb-16 sm:mb-18 lg:mb-24"><SiteNav/></div>
-    <router-view class="flex-grow" />
-    <SiteFooter/>
+  <div id="app" class="font-body text-gray-800">
+    <component :is="layout" />
   </div>
 </template>
 
 <script>
-import SiteNav from '@/components/SiteNav.vue';
-import SiteFooter from '@/components/SiteFooter.vue';
+import DefaultLayout from '@/views/layouts/DefaultLayout.vue';
 
 export default {
   name: 'App',
-  components: { SiteNav, SiteFooter },
-  metaInfo: {
-    title: 'The Ultimate Guide to COVID-19 Travel Restrictions',
-    titleTemplate: '%s | Project Travel Reopening',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'description', content: 'A free and open-source project by TourHero. Find the latest information for travel restrictions around the world during this COVID-19 pandemic. Travel safe!' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:image', content: `https://www.tourhero.com${process.env.BASE_URL}img/travel-reopening-cover.jpg` },
-      { name: 'og:image', content: `https://www.tourhero.com${process.env.BASE_URL}img/travel-reopening-cover.jpg` },
-    ],
+  components: {
+    DefaultLayout,
+  },
+  computed: {
+    layout() {
+      if (this.$route.name === 'OembedMap') {
+        return OembedLayout;
+      }
+      return DefaultLayout;
+    },
   },
 };
 </script>
